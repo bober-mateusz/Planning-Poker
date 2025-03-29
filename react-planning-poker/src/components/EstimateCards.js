1c5f3f1ffc68cc7ccfff9d54f585a6f7ccca1d68
Ethan <ethancaff@gmail.com>
2025-03-29 15:50:31 +0000

    fix: restored point system to previous state

--- react-planning-poker/src/components/EstimateCards.js
+++ react-planning-poker/src/components/EstimateCards.js
@@ -2,14 +2,10 @@ import * as React from 'react';
 import EstimateCard from './Cards/EstimateCard';
 import { Box } from '@mui/material';
 
-function EstimateCards() {
+function EstimateCards({handleOnClick}) {
   // TBD Different configs
   const estimates = ['?', '0.5', '1', '2', '3', '5', '8'];
 
-  //Place holder, tf is Ethans code?
-  const handleOnClick = (value) => {
-    console.log(value);
-  };
   return (
     <Box sx={{ display: 'flex', mx: 5, gap: 2 }}>
       {estimates.map((value) => (
