9eb8b87ce3bb15c246de6f462087bf62d2310a57
Conor Moroney <conorm18@gmail.com>
2025-04-02 21:20:33 +0100

    Initial Spring backend

---
+++ backend/src/main/java/com/planningpoker/backend/WebSocketConfig.java
@@ -0,0 +1,19 @@
+package com.planningpoker.backend;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@CrossOrigin
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new PokerWebSocketHandler(), "/poker")
+                .setAllowedOrigins("*"); // Allow frontend connections
+    }
+}
